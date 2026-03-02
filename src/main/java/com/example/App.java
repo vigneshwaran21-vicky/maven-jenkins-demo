0fa13bdf1b341acfd7902ae0e37488d8046fa32e
vigneshwaran21-vicky <vigneshwaran.2023b@vitstudent.ac.in>
2026-03-02 13:27:11 +0530

    Initial Maven Project

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,28 @@
+package com.example;
 
+public class App {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        App calc = new App();
+        System.out.println("Addition: " + calc.add(5,3));
+    }
+}
